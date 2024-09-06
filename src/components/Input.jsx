98f5943d9a8b398edf7e418647c84af5b2bbd828
Anna1719 <barinova_ann@mail.ru>
2024-09-06 17:45:09 +0300

    fix: input form fix

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,8 @@
 export const Input = ({label, error, handleChange, value, ...rest}) => {
     return (
        <div className='input_wrapper'>
-       <label htmlFor="email" style={error?{color:'red'}:{}} >{error?error:label}:</label>
-       <input {...rest} className="email_form" value={value} onChange={handleChange}/>
+       <label htmlFor="email" style={error ? {color:'red'} : {}} >{error ? error : label}:</label>
+       <input {...rest} className="input" value={value} onChange={handleChange}/>
        </div>
     )
 }
